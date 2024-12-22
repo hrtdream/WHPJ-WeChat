2fb184179e8003ee6c043374a9fe6d789235bcf0
Zhang Qilong <zhangcqypt@icloud.com>
2024-12-23 00:30:22 +0800

    add py script

---
+++ main.py
@@ -0,0 +1,83 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from datetime import datetime
+
+# 定义目标URL
+urls = [
+    "https://www.boc.cn/sourcedb/whpj/index.html",
+    "https://www.boc.cn/sourcedb/whpj/index_1.html",
+]
+
+
+# 创建一个函数来爬取数据
+def scrape_exchange_rates(url, target_currencies):
+    response = requests.get(url)
+    response.encoding = "utf-8"  # 确保中文字符正确解码
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        # 定位表格
+        table = soup.find_all("table")[1]
+        if not table:
+            print(f"No table found on {url}")
+            return []
+
+        rows = table.find_all("tr")
+        data = []
+        for row in rows[1:]:  # 跳过表头
+            cols = row.find_all("td")
+            cols = [col.text.strip() for col in cols]
+            # 筛选目标币种
+            if cols and cols[0] in target_currencies:
+                data.append(cols)
+        return data
+    else:
+        print(f"Failed to fetch {url}, status code: {response.status_code}")
+        return []
+
+
+# 发送汇率信息到微信
+def send_to_wechat(title):
+    params = {
+        "appkey": "482d185a324970fed1037e054416b735-wPQ7KTLd",
+        "title": title,
+    }
+    response = requests.get("https://cx.super4.cn/push_msg", params=params)
+
+    if response.status_code == 200:
+        print("消息发送成功")
+    else:
+        print(f"消息发送失败，状态码：{response.status_code}, 响应：{response.text}")
+
+
+# 主程序
+def main():
+    target_currencies = ["欧元", "新加坡元"]
+    all_data = {}
+    for url in urls:
+        data = scrape_exchange_rates(url, target_currencies)
+        if data[0][0] not in all_data:
+            all_data[data[0][0]] = data[0][4]
+
+    # 打印结果
+    if all_data:
+        print("汇率信息：")
+        for key, value in all_data.items():
+            print(key, value)
+    else:
+        print("未获取到数据。")
+
+    if float(all_data["欧元"]) < 760:
+        send_to_wechat(f"汇率降价，欧元：{all_data['欧元']}")
+    time.sleep(0.5)
+    if float(all_data["新加坡元"]) < 540:
+        send_to_wechat(f"汇率降价，新加坡元：{all_data['新加坡元']}")
+    time.sleep(0.5)
+    if datetime.now().hour == 18:
+        send_to_wechat(
+            f"{datetime.now().year}年{datetime.now().month}月{datetime.now().day}日，新加坡元：{all_data['新加坡元']}，欧元：{all_data['欧元']}"
+        )
+
+
+if __name__ == "__main__":
+    main()
